31bc5a682a8818c6b590f2c99cebc2264a3533f1
HakisIT <112759178+HakisIT@users.noreply.github.com>
2024-01-07 20:30:12 +0200

    program performance optimization, working with id as a dictionary key

--- main.py
+++ main.py
@@ -18,12 +18,24 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-mycursor.execute('SELECT english, russian, сzech FROM main')
+mycursor.execute('SELECT id FROM main')
 words = mycursor.fetchall()
+lst_index = []
+for i in words:
+    lst_index.append(i[0])
+
+def selection():
+    random_id = random.choice(lst_index)
+    sql_select_query = """SELECT * FROM main WHERE id = %s"""
+    mycursor.execute(sql_select_query, (random_id,))
+    word1 = mycursor.fetchall()
+    dct = {word1[0][0]:[word1[0][1], word1[0][2], word1[0][3]]}
+    return dct
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
+        # work with txt file
         # with open('D:\Project\English words\english_words.txt', 'r', encoding="utf8") as file:
         #     lines = list(map(str.strip, file.readlines()))
         #     dct = {}
@@ -32,19 +44,17 @@ class MyWidget(QtWidgets.QWidget):
         #             splt = i.split(' - ')
         #             dct.update({splt[0]:splt[1]})
         dct = {}
-        for i in words:
-            dct.update({i[0]:[i[1], i[2]]})
+        dct.update(selection())
 
         self.setWindowTitle('English Words')
         self.setWindowIcon(QtGui.QIcon('UkIcon.png'))
-        self.hello = list(dct)
         self.dct = dct
         self.button_next_word = QtWidgets.QPushButton()
         self.button_ru = QtWidgets.QPushButton()
         self.button_en = QtWidgets.QPushButton()
         self.button_cz = QtWidgets.QPushButton()
         self.text = QtWidgets.QLabel("Hello World")
-        self.text.setFont(QFont('Times', 35)) 
+        self.text.setFont(QFont('', 35))
         self.text.setAlignment(QtCore.Qt.AlignCenter)
         self.button_next_word.setStyleSheet("QPushButton"
                              "{"
@@ -63,7 +73,6 @@ class MyWidget(QtWidgets.QWidget):
         self.button_ru.setIcon(QtGui.QIcon('flag-for-united-kingdom.svg'))
         self.button_en.setIcon(QtGui.QIcon('flag-for-russia.svg'))
         self.button_cz.setIcon(QtGui.QIcon('czech-republic.png'))
-
         self.button_next_word.setIconSize(QSize(50,50))
         self.button_ru.setIconSize(QSize(50,50))
         self.button_en.setIconSize(QSize(50,50))
@@ -89,18 +98,19 @@ class MyWidget(QtWidgets.QWidget):
 
 
     def next_random_word(self):
-        self.rnd_word = random.choice(self.hello)
-        self.past_word.append(self.rnd_word)
-        self.text.setText(self.rnd_word)
-        
+        self.select = selection()
+        self.values = list(self.select.values())[0]
+        self.text.setText(self.values[0])
+
     def translate_en(self):
-        self.text.setText(self.dct.get(self.rnd_word)[0])
+        self.text.setText(self.values[1])
+
 
     def translate_cz(self):
-        self.text.setText(self.dct.get(self.rnd_word)[1])
+        self.text.setText(self.values[2])
 
     def translate_ru(self):
-        self.text.setText(self.past_word[-1])
+        self.text.setText(self.values[0])
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
